5541c5b91d03bb56fbb58c5d41cd1264fc1b2e12
DavidSalazar <jdrsalazar1025@gmail.com>
2025-08-13 11:43:31 -0600

    clase abstracta persona
    se crea con el fin de que hereden de ella, viene con metodos utilizados en la clase persona de zoomanager (es una copia)

---
+++ src/Personas/Persona.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personas;
+
+import Utils.UtilDate;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author UTN
+ */
+public abstract class Persona {
+     protected String id;
+    protected String name;
+    protected LocalDate birthDate;
+    protected String phone;
+    
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+    
+    public int getAge(){
+        return UtilDate.calculateAge(birthDate);
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
