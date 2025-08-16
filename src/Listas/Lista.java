38c918e0d6191aa05fdeaa848f5db92c7a386be2
Felipe0220 <lusalaspe@est.utn.ac.cr>
2025-08-16 14:41:00 -0600

    Paquete Listas y Clase Lista
    Se creo el paquete listas donde se creo una clase lista de tipo interfaz donde esta se creo y se le agregaron los metodos necesarios para utilizar en otras clases

---
+++ src/Listas/Lista.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Listas;
+
+/**
+ *
+ * @author Luisf
+ */
+public interface Lista <V> {
+    public boolean Eliminar(V v);
+    public V Buscar(Object placa);
+    public boolean Actualizar(V v);
+    boolean modificarReserva(V v);   
+    boolean cancelarReserva(V v);    
+    V buscarReservaPorCliente(Object idReserva);
+    boolean confirmarReserva(V v);
+}
+
