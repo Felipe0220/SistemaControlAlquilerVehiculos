0fff3bc14b174d4a099f9c5c6ca23832607b80eb
Kevinq <kevinquiros.m@gmail.com>
2025-08-24 14:13:31 -0600

    Clase empleado
    Se crea la clase empleado en el paquete persona

---
+++ src/Personas/Empleado.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personas;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author kevin
+ */
+public class Empleado extends Persona {
+     private String puesto;
+    private double salario;
+    public Empleado(String id, String name, LocalDate birthDate, String phone) {
+        super(id, name, birthDate, phone);
+        this.puesto = puesto;
+        this.salario = salario;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "puesto=" + puesto + ", salario=" + salario + '}';
+    }
+    
+}
