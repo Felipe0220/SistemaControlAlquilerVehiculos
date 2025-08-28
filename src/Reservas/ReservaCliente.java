1179ab1ca5f3d49b32d894d1cfd3d71989d82ee4
Kevinq <kevinquiros.m@gmail.com>
2025-08-28 00:20:39 -0600

    Gestion de Reservas
    Se crea el paquete de reservas y si trasladan las clases ,reserva y QueueReserva tambien se crea el InternalFram

--- src/Reservas/ReservaCliente.java
+++ src/Reservas/ReservaCliente.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Clientes;
+package Reservas;
 
+import Clientes.Cliente;
 import Excepciones.Excepcion;
 import Vehiculos.Estado;
 import static Vehiculos.Estado.Disponible;
